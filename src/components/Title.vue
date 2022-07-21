fd76cfcbe8cdd52d762f7e51bbb3045bf49dbc74
giomen88 <giorgia9288@virgilio.it>
2022-07-21 17:41:39 +0200

    Title ok

---
+++ src/components/Title.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>La mia prima app con Vue CLI!</h1>
+</template>
+
+<script>
+export default {
+    name: 'Title',
+};
+</script>
+
+<style>
+h1 {
+    text-align: center;
+    font-family: sans-serif;
+}
+</style>
